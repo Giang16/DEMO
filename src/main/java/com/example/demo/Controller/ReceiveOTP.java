ef8538f4c368dcecdc41e15ae41167cf3abcf655
EdW10 <pdoanthuannb96@gmail.com>
2023-12-08 16:51:42 +0700

    Sua cai Verify
    Sua cai verify ma mat ca buoi chieu ?

--- src/main/java/com/example/demo/Controller/ReceiveOTP.java
+++ src/main/java/com/example/demo/Controller/ReceiveOTP.java
@@ -5,6 +5,9 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.SocketOption;
+import java.nio.charset.StandardCharsets;
+
 import static com.example.demo.Function.EmailSender.GenOTP;
 
 @RestController
@@ -13,31 +16,39 @@ public class ReceiveOTP {
     @Autowired
     private EmailSender emailSender;
 
-    @GetMapping("/revOTP")
-    public String revOTP(@RequestBody String requestBody) {
-        JSONObject requestJSON = new JSONObject(requestBody);
-        String email;
-        try{
-            email = requestJSON.getString("email");
-            emailSender.sendOTPEmail(email,"OTP",GenOTP(email));
-            return "1";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "0";
-        }
-    }
-
-    @GetMapping("/verifyOTP")
-    public String verifyOTP(@RequestParam(name = "otp", required = false) String otp,
-                            @RequestParam(name = "email", required = false) String email
-    ) {
-        if (otp != null && email != null) {
-            if (GenOTP(email).equals(otp)) {
+    @RequestMapping("/revOTP")
+    public String revOTP(@RequestParam(name = "email", required = false) String email) {
+        if(email != null) {
+            try{
+                emailSender.sendOTPEmail(email,"OTP",GenOTP(email));
                 return "1";
-            } else {
-                return "-1";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "0";
             }
         } else {
+            return "-1";
+        }
+
+    }
+
+    @RequestMapping("/verifyOTP")
+    public String verifyOTP(@RequestBody String requestBody) {
+        JSONObject requestJSON = new JSONObject(requestBody);
+        try {
+            String otp = requestJSON.getString("otp");
+            String email = requestJSON.getString("email");
+            if (otp != null && email != null) {
+                if (GenOTP(email).equals(otp)) {
+                    return "1";
+                } else {
+                    return "-1";
+                }
+            } else {
+                return "0";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
             return "0";
         }
     }
