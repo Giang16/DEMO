e44deff7bbcf3fd58f9c6cc8181c89b4d5ffc5f7
Giang16 <bao16011304@gmail.com>
2023-12-12 03:06:24 +0700

    delete

--- src/main/java/com/example/demo/JPARepository/TamTruRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.JPARepository;
-
-import com.example.demo.Model.TamTru;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TamTruRepository extends JpaRepository<TamTru, String> {
-
-}
--- src/main/java/com/example/demo/JPARepository/TamVangRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.JPARepository;
-
-import com.example.demo.Model.TamVang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TamVangRepository extends JpaRepository<TamVang, String> {
-
-}
